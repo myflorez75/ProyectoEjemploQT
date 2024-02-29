7af75784ed0f789a0fa09cdf31e6c2632f16151f
myflorez75 <160801086+myflorez75@users.noreply.github.com>
2024-02-28 19:27:54 -0500

    Punto1_laboratorio

--- Ejer1.cpp
+++ Ejer1.cpp
@@ -12,8 +12,18 @@ int Ejercicio1(){
         || caracter == 'A'|| caracter == 'E' || caracter == 'I' || caracter == 'O' || caracter == 'U'){
         cout << caracter << " es una vocal"<< endl;
     }
-    else{
+    else if (caracter == 'b'|| caracter == 'c' || caracter == 'd' || caracter == 'f' || caracter == 'g'
+             || caracter == 'h'|| caracter == 'j' || caracter == 'k' || caracter == 'l' || caracter == 'm'|| caracter == 'p' || caracter == 'q' || caracter == 'n' || caracter == 'r'|| caracter == 's'
+             || caracter == 't' || caracter == 'v' || caracter == 'w' || caracter == 'x'
+             || caracter == 'y' || caracter == 'z'
+             || caracter == 'B'|| caracter == 'C' || caracter == 'D' || caracter == 'F' || caracter == 'G'
+             || caracter == 'H'|| caracter == 'J' || caracter == 'K' || caracter == 'L' || caracter == 'M'|| caracter == 'P' || caracter == 'Q' || caracter == 'N' || caracter == 'R'|| caracter == 'S'
+             ||caracter == 'T' || caracter == 'V' || caracter == 'W' || caracter == 'X'
+             || caracter == 'Y' || caracter == 'Z'){
         cout << caracter << " es una consonante"<< endl;
+    }
+    else{
+        cout << caracter << " no es una letra"<< endl;
     }
 
     return 0;
